1a98473f48c5579545eecd819f17c9bd7ce9ac9e
7ee7a2a997f561cca7659fa7396f8136e186c66b <design@sanchezmauricio.com>
2015-04-29 15:14:43 -0400

    footer working added some functionality to the links

--- wordpress/wp-content/themes/mauricio/footer.php
+++ wordpress/wp-content/themes/mauricio/footer.php
@@ -5,19 +5,30 @@
 
       <footer id="footer-mauricio" class="navbar-inverse">
         <div class="container">
-            <section class="col-md-6 footer-contact">
-                <div id="other-content">
-                  <img id="github" src="http://localhost/wp-content/uploads/2014/01/github-front.png" height="50" width="50">
-                  <img id="linkedin" src="http://localhost/wp-content/uploads/2014/01/linkedin-front.png" height="50" width="50">
-                  <img id="blog" src="http://localhost/wp-content/uploads/2014/01/blog_front.png" height="50" width="50">
-                </div>
-            </section>
-            <section class="col-md-6 footer-contact">
+            <!-- <section class="col-md-6 footer-contact"> -->
+                <!-- <div id="other-content"> -->
+                  <section class="contact-images">
+                    <a href="https://github.com/sheva29" target="_blank">
+                      <img id="github" src="http://localhost/wp-content/uploads/2015/03/github_logo.png" height="50" width="50">
+                    <a/>
+                    <a href="https://www.linkedin.com/profile/view?id=51325490&trk=nav_responsive_tab_profile" target="_blank">
+                      <img id="linkedin" src="http://localhost/wp-content/uploads/2015/03/linkedin_logo.png" height="50" width="50">
+                    <a/>
+                    <a href="http://localhost/?page_id=60" target="_blank">
+                      <img id="blog" src="http://localhost/wp-content/uploads/2015/03/wordpress_logo.png" height="50" width="50">
+                    <a/>
+                    <a href="https://twitter.com/maurifique" target="_blank">
+                      <img id="twitter" src="http://localhost/wp-content/uploads/2015/03/twitter_logo.png" heigh="50" width="50">
+                    <a/>
+                </section>
+                <!-- </div> -->
+            <!-- </section> -->
+<!--             <section class="col-md-6 footer-contact">
                 <div id="personal-description"> 
                   <h1 id="personal-title">The Designer...</h1>
                   <p id="personal-paragraph"> Bla Bla Bla Bla Bla Bla </p>
                 </div>  
-            </section>
+            </section> -->
             <section class="footer-note">
               <p id="bottom-content"> &copy; <?php echo date( 'Y' ); ?> | Mauricio Sanchez | All rights reserved. </p>
             </section>
