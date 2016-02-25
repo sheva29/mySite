db2553f237e50f5159ac106f1acdce223da05d2a
sheva29 <design@sanchezmauricio.com>
2016-02-25 05:05:33 +0000

    Added image

--- page-about.php
+++ page-about.php
@@ -1,4 +1,4 @@
-<!-- Always use the prefix page_'pagename'.php when adding a custom page to your wordpress -->
+ring <!-- Always use the prefix page_'pagename'.php when adding a custom page to your wordpress -->
 <!-- About Page -->
 <?php
 //Use this to add a custom page
@@ -17,8 +17,9 @@ $category_posts = new WP_Query( $arg );
 $classes = array('col-md-3 ', 'image-container ');
 $terms = get_terms('category', array('parent' => 9));
 
-?>
+?>	
 
+	<img id="about-image" src="http://www.sanchezmauricio.com/wp-content/uploads/2016/02/mauricio_about.jpg"></img>
 	<p class="about-paragraph"> I am a Colombian designer and technologist. I have an MFA in Design and Technology from Parsons School of Design and received my bachelors in Industrial Design from ICESI University. I currently work with Spies & Assassins where I tinker with algorithms and data as a Product Architect and Experiential Developer. </p>
 
 	<p class="about-paragraph">I moved to New York City in 2008 to work at Glide-Inc, an engineering firm, where I consulted on design and the development of products and furniture. After two years at Glide, I went to work at Martel Design focusing on digital fabrication and customized furniture. My current work focuses on exploring the junction of physical and digital through creative coding and digital experiences to deliver human centric products that always meet client expectations. </p>
