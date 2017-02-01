a8d903ac1703d4ed38bec6e9a283844c05b30b4c
sheva29 <design@sanchezmauricio.com>
2017-02-01 06:04:21 +0000

    Footer almost completed

--- footer.php
+++ footer.php
@@ -3,32 +3,31 @@
         <div class="container">
             <section class="contact-images row">
 
-              <div class="col-xs-4 col-lg-4 footer-column-container"> 
-                <p class="contact-p others-title"> CONTACT </p>
+              <div class="col-xs-3 col-lg-3 footer-column-container"> 
+                <p class="others-title"> CONTACT </p>
                 <p class="footer-email"> design[at]sanchezmauricio.com</p>
-                <p id="bottom-content"> &copy; Mauricio Sanchez | <?php echo date( 'Y' ); ?> </p>
               </div>
 
-              <div class="col-xs-4 col-lg-4 footer-column-container"> 
-                <p class="contact-p others-title"> SOCIAL MEDIA </p>
-                <a href="https://twitter.com/maurifique" target="_blank">
-                  <img id="twitter" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/twitter_logov2.0.png" >
-                </a>
-                <a href="https://www.linkedin.com/profile/view?id=51325490&trk=nav_responsive_tab_profile" target="_blank">
-                  <img id="linkedin" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/linkedin-logov2.0.png" >
-                </a>
+              <div class="col-xs-9 col-lg-9 footer-column-container social-elements-container"> 
+                <!-- <p class="others-title"> SOCIMEDIAAL  </p> -->
+                <div class="social-elements">
+                    <a class=href="https://twitter.com/maurifique" target="_blank">
+                      <img id="twitter" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/twitter_logov2.0.png" >
+                    </a>
+                    <a href="https://www.linkedin.com/profile/view?id=51325490&trk=nav_responsive_tab_profile" target="_blank">
+                      <img id="linkedin" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/linkedin-logov2.0.png" >
+                    </a>
+                      <a href="https://github.com/sheva29" target="_blank">
+                      <img id="github" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/github_logov2.0.png">
+                    </a>
+                    <a href="http://www.sanchezmauricio.com/thinkingprocess/" target="_blank">
+                      <img id="blog" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/wordpress_logov2.0.png" ">
+                    </a>
+                </div>
               </div>
-
-              <div class="col-xs-4 col-lg-4 footer-column-container">
-                <p class="contact-p others-title"> CODE + THINKING </p> 
-                <a href="https://github.com/sheva29" target="_blank">
-                  <img id="github" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/github_logov2.0.png">
-                </a>
-                <a href="http://www.sanchezmauricio.com/thinkingprocess/" target="_blank">
-                  <img id="blog" src="http://www.sanchezmauricio.com/wp-content/uploads/2017/01/wordpress_logov2.0.png" ">
-                </a>
-              </div> 
-
+                <div class="copy-right">
+                    <p id="bottom-content"> &copy; Mauricio Sanchez | <?php echo date( 'Y' ); ?> </p>
+                </div>
           </section>
         </div>       
       </footer>
